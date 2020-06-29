12e9097d3701df65cf94f1469090dd3d4de154c1
Elijah wines <elijahwines84@yahoo.com>
2020-06-29 13:58:15 +0000

    Automatically backed up by Learn

--- db/migrate/20200629135549_create_appointments.rb
+++ db/migrate/20200629135549_create_appointments.rb
@@ -2,8 +2,10 @@ class CreateAppointments < ActiveRecord::Migration[5.0]
   def change
     create_table :appointments do |t|
       t.datetime :appointment_datetime
-      t.string :patient
-      t.string :doctor
+      t.integer :patient_id
+      t.integer :doctor_id
+
+      t.timestamps
     end
   end
 end
